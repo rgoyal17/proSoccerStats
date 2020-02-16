1f804a6f9e55458c849be8f9eb0b7e7ed994713e
rgoyal17 <rgoyal17@uw.edu>
2020-02-15 23:28:16 -0800

    updates

--- js/index.js
+++ js/index.js
@@ -21,7 +21,11 @@ function clubValues(arrayData) {
 function addRows(arrayData) {
   let tableBodyElement = document.querySelector('#table-body');
   tableBodyElement.innerHTML = '';
-  for (let i = 0; i < 10; i++) {
+  let end = arrayData.length;
+  if (arrayData.length >= 10) {
+    end = 10;
+  }
+  for (let i = 0; i < end; i++) {
     let rowElement = document.createElement('tr');
     let th = document.createElement('th');
     th.textContent = i + 1;
@@ -30,6 +34,9 @@ function addRows(arrayData) {
     let img = document.createElement('img');
     img.src = arrayData[i].Photo;
     img.alt = arrayData[i].Name;
+    img.onerror = function() {
+      td1.textContent = "Image Unavailable";
+    }
     img.classList.add('player-img');
     td1.appendChild(img);
     rowElement.appendChild(td1);
@@ -65,9 +72,56 @@ function addRows(arrayData) {
 }
 
 d3.csv("data.csv").then(function(data) {
+  console.log(data);
   clubValues(data);
   addRows(data);
+  filterData(data);
 });
+
+// filter data based on search conditions
+function filterData(arrayData) {
+  document.querySelector('#search-btn').addEventListener('click', function(event) {
+    event.preventDefault();
+    let nameValue = document.querySelector('#name').value;
+    let nationalityValue = document.querySelector('#nationality').value;
+    let clubValue = document.querySelector('#club').value;
+    let positionValue = document.querySelector('#position').value;
+    let footValue = document.querySelector('#foot').value;
+    let ageValue = document.querySelector('#age').value;
+    let filteredArray = arrayData;
+    if (nameValue != "") {
+      filteredArray = filteredArray.filter(function(item){
+        return item.Name.includes(nameValue);
+      });
+    }
+    if (!nationalityValue.includes("Choose Country")) {
+      filteredArray = filteredArray.filter(function(item){
+        return item.Nationality.includes(nationalityValue);
+      });
+    }
+    if (!clubValue.includes("Choose Club")) {
+      filteredArray = filteredArray.filter(function(item){
+        return item.Club.includes(clubValue);
+      });
+    }
+    if (!positionValue.includes("Choose Position")) {
+      filteredArray = filteredArray.filter(function(item){
+        return item.Position.includes(positionValue);
+      });
+    }
+    if (!footValue.includes("Choose Foot")) {
+      filteredArray = filteredArray.filter(function(item){
+        return item.Preferred_Foot.includes(footValue);
+      });
+    }
+    if (ageValue != "") {
+      filteredArray = filteredArray.filter(function(item){
+        return item.Age.includes(ageValue);
+      });
+    }
+    addRows(filteredArray);
+  })
+}
 
 // fetch("https://api.sportradar.us/soccer-images-t3/ap/world-cup/headshots/players/2016/manifest.json?api_key=jkkmdgd7nyr8gyqgajppxxc7")
 //     .then(function(response) {
